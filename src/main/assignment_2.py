e41d402fbcfbe1da92736ebef68d0d60ad8c20c4
AlannaFey <alanna.fey.hill@gmail.com>
2023-02-21 21:39:32 -0500

    added comments and fixed file organization

--- src/main/assignment_2.py
+++ src/main/assignment_2.py
@@ -1,5 +1,11 @@
+# Alanna Hill
+# al538601
+# COT4500 Spring 2023
+
 import numpy as np;
 
+# This function uses nevilles method to find the second degree
+# interpolating value for f(x) given x and y points
 def neville(x_points, y_points, x):
     num_points = len(x_points) 
     matrix = np.zeros((num_points,num_points))
@@ -14,6 +20,8 @@ def neville(x_points, y_points, x):
             matrix[i][j] = (first - second) / denom
     return matrix[num_points - 1][num_points - 1]
 
+# This function uses newton's forward method and returns a matrix
+# based off of the x and y points provided
 def newton(x_points, y_points):
     size = len(x_points)
     matrix  = np.zeros((size, size))
@@ -28,7 +36,8 @@ def newton(x_points, y_points):
             op = numer / denom
             matrix[i][j] = op
     return matrix
-
+# This function takes in a matrix and a set of points and a value
+# to aproximate and aproximates it
 def aprox(matrix, x_points, y_points, x):
     rec_x = 1
     rec_px = y_points[0]
@@ -39,7 +48,8 @@ def aprox(matrix, x_points, y_points, x):
         mult = coef * rec_x
         rec_px += mult
     return rec_px
-
+# This function applies divided diferences to a Hermite aproximation
+# matrix
 def apply_div_dif(matrix):
     size = len(matrix)
     for i in range(2, size):
@@ -53,7 +63,7 @@ def apply_div_dif(matrix):
             den = matrix[i][0] - matrix[i - (j - 1)][0]
             matrix[i][j] = num / den
     return matrix
-
+# This function calculates a Hermite aproximation matrix
 def hermite(x_points, y_points, f1x):
     num_points = len(x_points)
     matrix = np.zeros((num_points * 2, num_points * 2))
@@ -68,12 +78,14 @@ def hermite(x_points, y_points, f1x):
     return matrix
 
 
-def findMatrix(vals):
+def findMatrix(x_points, y_points):
+    num_points = len(x_points)
     return
 
 def findVector(x, vals):
     return
 
+# This is the main function which runs everything
 def main():
     np.set_printoptions(precision=7,suppress=True,linewidth=100)
     x_points1 = [3.6, 3.8, 3.9]
